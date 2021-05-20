a13089037ccfa6e3da5431b576fa42fe8edd3078
myron-afk <davidpaul.mironescu@gmail.com>
2021-05-20 15:58:46 +0300

    Update PowersConfig2.java

--- TeamCode2/src/main/java/org/firstinspires/ftc/teamcode/faraRR/PowersConfig2.java
+++ TeamCode2/src/main/java/org/firstinspires/ftc/teamcode/faraRR/PowersConfig2.java
@@ -5,9 +5,9 @@ public class PowersConfig2 {
     public static final double COEFF_SPEED_HIGH = 1;
     public static final double COEFF_SPEED_LOW = 0.7;
 
-    public static final double INTAKE_SUCK = 0.85;
+    public static final double INTAKE_SUCK = 1; //0.85;
 
-    public static final double LANSAT_POWER = 0.675;
+    public static final double LANSAT_POWER = 0.6; //0.675;
 
     public static final double INTAKE2_RIGHT = 1;
     public static final double INTAKE2_LEFT = 0;
